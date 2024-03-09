785f14fb7fbcfff8a869c2f80fecaeeda75c598d
Michiel Eeckhout <105851314+kingbasilisk13@users.noreply.github.com>
2024-03-09 16:04:03 +0100

    added imgui to project

--- Minigin/InputManager.cpp
+++ Minigin/InputManager.cpp
@@ -1,8 +1,13 @@
 #include <SDL.h>
 #include "InputManager.h"
 
+#include <SDL_syswm.h>
+
+#include <backends/imgui_impl_sdl2.h>
+
 bool dae::InputManager::ProcessInput()
 {
+	
 	SDL_Event e;
 	while (SDL_PollEvent(&e)) {
 		if (e.type == SDL_QUIT) {
@@ -14,7 +19,7 @@ bool dae::InputManager::ProcessInput()
 		if (e.type == SDL_MOUSEBUTTONDOWN) {
 			
 		}
-		// etc...
+		ImGui_ImplSDL2_ProcessEvent(&e);
 	}
 
 	return true;
